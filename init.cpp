58309b7b73acda99841bbab86c091ebbee001950
b940417 <gui940417@gmail.com>
2024-05-19 16:01:55 +0800

    init

---
+++ init.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std;
+
+class Complex {
+public:
+	Complex();                                     // Default constructor
+	Complex(double realPart, double imagPart);     // Constructor with two parameters
+	Complex(double realPart);                      // Constructor with single parameter
+
+	bool operator ==(const Complex& other) const;      // Overloaded operators : ==
+	Complex operator +(const Complex& other) const;    // +
+	Complex operator -(const Complex& other) const;    // -
+	Complex operator *(const Complex& other) const;    // *
+
+	friend istream& operator >>(istream& in, Complex& c);           // Input
+	friend ostream& operator <<(ostream& out, const Complex& c);    // Output
+private:
+	
+};
+
+int main() {                                // Test program
+	
+	return 0;
+}
+
+
+bool Complex::operator ==(const Complex& other) const {
+	
+}
+
+Complex Complex::operator +(const Complex& other) const {
+	
+}
+
+Complex Complex::operator -(const Complex& other) const {
+	
+}
+
+Complex Complex::operator *(const Complex& other) const {
+	
+}
+
+istream& operator >>(istream& in, Complex& c) {
+	
+}
+
+ostream& operator <<(ostream& out, const Complex& c) {
+	
+}
