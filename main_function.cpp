7f472d3c1fd2494eb2bd9e1bdbbd9c691119468c
Eason-zyr <line23938718@gmail.com>
2024-05-19 18:22:27 +0800

    feat:main function and private variables

--- main_function.cpp
+++ main_function.cpp
@@ -16,35 +16,50 @@ public:
 	friend istream& operator >>(istream& in, Complex& c);           // Input
 	friend ostream& operator <<(ostream& out, const Complex& c);    // Output
 private:
-	
+	double real;
+	double imaginary;
 };
 
 int main() {                                // Test program
-	
+	Complex a(3, 4);
+	Complex b(1, 2);
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl << endl;
+
+	cout << "a == b: " << (a == b) << endl;
+	cout << "a + b: " << a + b << endl;
+	cout << "a - b: " << a - b << endl;
+	cout << "a * b: " << a * b << endl << endl;
+
+	Complex c;
+	cin >> c;
+	cout << "You entered: " << c << endl;
+
+	system("pause");
 	return 0;
 }
 
-
 bool Complex::operator ==(const Complex& other) const {
-	
+
 }
 
 Complex Complex::operator +(const Complex& other) const {
-	
+
 }
 
 Complex Complex::operator -(const Complex& other) const {
-	
+
 }
 
 Complex Complex::operator *(const Complex& other) const {
-	
+
 }
 
 istream& operator >>(istream& in, Complex& c) {
-	
+
 }
 
 ostream& operator <<(ostream& out, const Complex& c) {
-	
+
 }
